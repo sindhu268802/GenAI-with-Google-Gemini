175174bbf95604c29013074b40ea4de7ac6568cf
Sindhu Barathi <82019642+sindhu268802@users.noreply.github.com>
2025-08-19 15:10:16 +0800

    Add files via upload

---
+++ End to End LLM app using Gemini Pro/vision.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv 
+load_dotenv() # load all environment variables from .env file
+
+import streamlit as st
+import os
+
+import google.generativeai as genai
+from PIL import Image
+
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+## Function to load Gemini model and get respones
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+def get_gemini_response(input,image):
+    if input!="":
+       response = model.generate_content([input,image])
+    else:
+       response = model.generate_content(image)
+    return response.text
+
+##initialize streamlit app
+
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
+    st.write(response)
---
+++ End to End Multi Language Invoice Extractor using Gemini Pro LLM model/app.py
@@ -0,0 +1,61 @@
+from dotenv import load_dotenv
+load_dotenv() # load all environment variables from .env file
+
+import os
+import streamlit as st
+from PIL import Image
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("AIzaSyBJqqtZCQRHKUhISkfRKZEi1jx7MrtX9UI"))
+
+# Initialize the Gemini Pro Vision model
+model = genai.GenerativeModel('gemini-2.5-pro')
+
+#Function to load gemini pro vision
+
+def get_gemini_response(input, image, prompt):
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+def input_image_details(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+# initialize stramlit app
+
+st.set_page_config(page_title="Multi Language Invoice Extractor")
+st.header("Multi Language Invoice Extractor")
+input=st.text_input("Input Prompt: ", key="input")
+uploaded_file = st.file_uploader("Choose an image of the invoice...", type=["jpg", "jpeg", "png"])
+image=""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+submit=st.button("Tell me about the invoice")
+
+input_prompt="""
+    You are an expert in understanding invoices.
+    You will receive input images as invoices &
+    you will have to answer questions based on the input image
+    """
+
+# if submit button is clicked
+if submit:
+    image_data = input_image_details(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
