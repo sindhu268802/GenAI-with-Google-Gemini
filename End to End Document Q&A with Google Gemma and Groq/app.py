5051758db0c3a2b185c9ade559cdf60daaadc86a
Sindhu Barathi <82019642+sindhu268802@users.noreply.github.com>
2025-08-24 13:14:28 +0800

    Add files via upload

---
+++ End to End Document Q&A with Google Gemma and Groq/app.py
@@ -0,0 +1,71 @@
+import os
+import streamlit as st
+
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import create_retrieval_chain
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## load the GROQ and Google API keys
+groq_api_key = os.getenv("GROQ_API_KEY")
+os.environ['GOOGLE_API_KEY'] = os.getenv("GOOGLE_API_KEY")
+
+st.title("Gemma Model Document Q&A")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model="gemma2-9b-it")
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+   
+"""
+)
+
+def vector_embeddings():
+    
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./us_census")
+        st.session_state.docs=st.session_state.loader.load()
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=300)
+        st.session_state.final_document=st.session_state.text_splitter.split_documents(st.session_state.docs)
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_document,st.session_state.embeddings)  
+prompt1=st.text_input("Enter Your Question From Doduments")
+
+
+if st.button("Documents Embedding"):
+    vector_embeddings()
+    st.write("Vector Store DB Is Ready")
+
+import time
+
+
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':prompt1})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
